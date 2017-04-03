33ea9105a161ef182c5e6354b285cf80d943b4ec
isapcosta <ihpcosta@hotmail.com>
2017-04-03 14:08:21 -0300

    Fase 1 concluida e fase 2 (1 e 2)

--- inspermons1.py
+++ inspermons1.py
@@ -1,29 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 31 10:30:16 2017
+import json
+def mostra_ipmon(ipmon):
+    print("Inspermon : {0}".format(ipmon["nome"]))
+    print("poder = {0}".format(ipmon["poder"]))
+    print("vida = {0}".format(ipmon["vida"]))
+    print("defesa = {0}\n".format(ipmon["defesa"]))
 
-@author: isadora
-"""
+    return  
 
-import random
-picaxu={"nome" : "picaxu",
-"poder" : 20,
-"vida" : 200,
-"defesa" : 12}
-	
-xamando={"nome" : "xamando",
-"poder" : 25,
-"vida" : 100,
-"defesa" : 10}
-	
-pidijet={"nome" : "pidijet",
-"poder" : 15,
-"vida" : 300,
-"defesa" : 10}
-import 
+with open('inspermons.json') as arquivo:
+    inspermons = json.load(arquivo)
+for i in range(3):
+    print(mostra_ipmon(inspermons[i]))
+    
 
-listadeinspermons={"picaxu":picaxu,'xamando':xamando,"pigijet":pidijet}
-print(listadeinspermons)
+insperdex=[]
+listadeinspermons={"picaxu":inspermons[0],'xamando':inspermons[1],"pigijet":inspermons[2]}
 inspermoninicial=input("Qual seu inspermon inicial da lista?")
 
 i=0
@@ -34,28 +25,47 @@ while i==0:
         break
     elif inicio=="passear":	
         inimigo=random.choice(list(listadeinspermons.keys()))
+        insperdex.append(inimigo)
     
-        print("O seu inimigo de batalha é:",inimigo)
+        print("Você acabou de encontrar o:",inimigo)
         VidaJogador=listadeinspermons[inspermoninicial]["vida"]
         VidaOponente=listadeinspermons[inimigo]["vida"]
         PoderJogador=listadeinspermons[inspermoninicial]["poder"]
         PoderOponente=listadeinspermons[inimigo]["poder"]
         DefesaJogador=listadeinspermons[inspermoninicial]["defesa"]
         DefesaOponente=listadeinspermons[inimigo]["defesa"]
-        while VidaJogador > 0 and VidaOponente > 0:
-            VidaOponente = VidaOponente - ( PoderJogador - DefesaOponente ) 
-            if VidaOponente <= 0:
-                print("Você ganhou a batalha!")
-            else:
-                VidaJogador = VidaJogador - ( PoderOponente - DefesaJogador )
-                
-            if VidaJogador <= 0:        
-                print("Voce perdeu")
-                
-
+        batalha=input("Você quer batalhar ou tentar fugir?")
+        opcoes=['escapou','semsorte']
+        if batalha=="batalhar":
+            while VidaJogador > 0 and VidaOponente > 0:
             
-        
-        
-        
-        
-        
+                
+                VidaOponente = VidaOponente - ( PoderJogador - DefesaOponente ) 
+                
+                if VidaOponente <= 0:
+                    print("Você derrotou o : {0}".format(inimigo))
+                else:
+                    VidaJogador = VidaJogador - ( PoderOponente - DefesaJogador )
+                    
+                if VidaJogador <= 0:        
+                    print("Você perdeu! para o : {0}".format(inimigo))
+                    
+        if batalha=="fugir":
+            escolha=random.choice(opcoes)
+            if escolha=="escapou":
+                print("Ufa,Você escapou")
+                continue
+            if escolha=="semsorte":
+                print("Você perdeu sua vez de jogar")
+                while VidaJogador > 0 and VidaOponente > 0:
+                    VidaJogador = VidaJogador - ( PoderOponente - DefesaJogador )
+                    if VidaJogador <= 0:        
+                        print("Que pena,Você perdeu para o : {0}".format(inimigo))
+                    
+                    else:
+                        VidaOponente = VidaOponente - ( PoderJogador - DefesaOponente ) 
+                    
+                    if VidaOponente <= 0:
+                        print("Você derrotou o : {0}".format(inimigo))
+
+print("Estes foram os inspermons que cruzaram sua jornada",set(insperdex))
